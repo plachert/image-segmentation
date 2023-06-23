5594a1b39b86bfd3eaf7790abe0b7d9bc5d3a085
plachert <pmlachert@gmail.com>
2023-06-23 22:16:23 +0200

    Refactor with precommit

--- src/model/architectures/unet.py
+++ src/model/architectures/unet.py
@@ -1,4 +1,5 @@
 """This module provides implementation of a modified U-Net architecture."""
+from __future__ import annotations
 
 import torch
 import torch.nn as nn
@@ -8,34 +9,48 @@ class ContractionBlock(nn.Module):
     def __init__(self, input_chans=3, output_chans=64):
         super().__init__()
         self.conv_relu = nn.Sequential(
-            nn.Conv2d(input_chans, output_chans, (3, 3), padding=1, bias=False),
+            nn.Conv2d(
+                input_chans, output_chans,
+                (3, 3), padding=1, bias=False,
+            ),
             nn.BatchNorm2d(output_chans),
             nn.ReLU(inplace=True),
-            nn.Conv2d(output_chans, output_chans, (3, 3), padding=1, bias=False),
+            nn.Conv2d(
+                output_chans, output_chans,
+                (3, 3), padding=1, bias=False,
+            ),
             nn.BatchNorm2d(output_chans),
             nn.ReLU(inplace=True),
         )
         self.maxpool = nn.MaxPool2d((2, 2), stride=2)
-        
+
     def forward(self, image):
         copy = self.conv_relu(image)
         out = self.maxpool(copy)
         return copy, out
 
-    
+
 class ExpansionBlock(nn.Module):
     def __init__(self, input_chans=1024, output_chans=512):
         super().__init__()
-        self.upsample = nn.ConvTranspose2d(input_chans, output_chans, (4, 4), stride=2, padding=1)
+        self.upsample = nn.ConvTranspose2d(
+            input_chans, output_chans, (4, 4), stride=2, padding=1,
+        )
         self.conv_relu = nn.Sequential(
-            nn.Conv2d(input_chans, output_chans, (3, 3), padding=1, bias=False),
+            nn.Conv2d(
+                input_chans, output_chans,
+                (3, 3), padding=1, bias=False,
+            ),
             nn.BatchNorm2d(output_chans),
             nn.ReLU(inplace=True),
-            nn.Conv2d(output_chans, output_chans, (3, 3), padding=1, bias=False),
+            nn.Conv2d(
+                output_chans, output_chans,
+                (3, 3), padding=1, bias=False,
+            ),
             nn.BatchNorm2d(output_chans),
             nn.ReLU(inplace=True),
         )
-        
+
     def forward(self, copy, image):
         upsampled = self.upsample(image)
         concatenated = torch.concat((copy, upsampled), axis=1)
@@ -50,13 +65,15 @@ class UNet(nn.Module):
         self.level2_down = ContractionBlock(input_chans=64, output_chans=128)
         self.level3_down = ContractionBlock(input_chans=128, output_chans=256)
         self.level4_down = ContractionBlock(input_chans=256, output_chans=512)
-        self.bottom = ContractionBlock(input_chans=512, output_chans=1024) # TODO: unused maxpool here
+        self.bottom = ContractionBlock(
+            input_chans=512, output_chans=1024,
+        )  # TODO: unused maxpool here
         self.level4_up = ExpansionBlock(input_chans=1024, output_chans=512)
         self.level3_up = ExpansionBlock(input_chans=512, output_chans=256)
         self.level2_up = ExpansionBlock(input_chans=256, output_chans=128)
         self.level1_up = ExpansionBlock(input_chans=128, output_chans=64)
         self.reduce_channels = nn.Conv2d(64, n_classes, (1, 1))
-        
+
     def forward(self, image):
         # Contraction
         copy1, out = self.level1_down(image)
@@ -72,9 +89,9 @@ class UNet(nn.Module):
         out = self.level1_up(copy1, out)
         out = self.reduce_channels(out)
         return out
-    
-    
-if __name__ == "__main__":
-    a = torch.rand(1, 3, 224 ,224)
+
+
+if __name__ == '__main__':
+    a = torch.rand(1, 3, 224, 224)
     m = UNet()
     print(m(a).shape)
